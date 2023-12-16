3d9a823993772a7a74d68d784d28234b87de5180
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-12-16 23:55:29 +0530

    Create IIFE.js

---
+++ Functions_in_JS/IIFE.js
@@ -0,0 +1,29 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+
+
+
+
+
+
+
+( ()=>{
+    console.log("hello b")
+})();
+
+( (a)=>{
+    console.log(`hello b ${a}`)
+})("Database");
+
+(()=>(console.log("hello"))
+)();
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+// Immediately Invoked Function Expressions (IIFE)
+console.log("Welcome to Programiz!");
